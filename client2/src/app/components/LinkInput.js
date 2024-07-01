88b1a3895b0c86eb222b9a3b639a154eec01e03e
tyler <t198@bu.edu>
2024-07-01 15:13:22 -0400

    migrate react app to nextjs

---
+++ client2/src/app/components/LinkInput.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const LinkInput = ({ onSubmit }) => {
+
+    const [link, setLink] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submitting");
+        setLink('');
+    }
+
+    return (
+        <div className="container mt-5">
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="jobLink">Job Listing Link</label>
+                    <input
+                        type="url"
+                        className="form-control"
+                        id="jobLink"
+                        placeholder="Enter job listing link"
+                        value={link}
+                        onChange={(e) => setLink(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={onSubmit}>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default LinkInput;
---
+++ client2/src/app/page.js
@@ -0,0 +1,31 @@
+"use client";
+import './App.module.css';
+import { useEffect, useState } from 'react';
+import NavBar from './components/Navbar';
+import LinkInput from './components/LinkInput';
+import JobTable from './components/JobsTable';
+
+function App() {
+  const [jobs, setJobs] = useState([]);
+
+  const handleLinkSubmit = (link) => {
+    const newJob = {
+      title: 'Software Engineer',
+      company: 'Google',
+      location: 'Mountain View, CA',
+      pay: '1,000,000',
+      link: link
+    };
+    setJobs(prevJobs => [...prevJobs, newJob]);
+  };
+
+  return (
+    <>
+      <NavBar />
+      <LinkInput onSubmit={handleLinkSubmit} />
+      <JobTable jobs={jobs} />
+    </>
+  );
+}
+
+export default App;
